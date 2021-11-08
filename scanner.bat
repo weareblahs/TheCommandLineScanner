ffd59d9fe7dd568a1e16c5729e15ec3f732c366c
Tan Yu Xuan <37889443+weareblahs@users.noreply.github.com>
2021-11-08 23:17:14 +0800

    New Scanner Stuffs™
     - config.ini used with scanner.bat with ini.cmd (https://stackoverflow.com/questions/2866117/windows-batch-script-to-read-an-ini-file)
     - Added Timer option

--- scanner.bat
+++ scanner.bat
@@ -1,25 +1,42 @@
 @echo off
-cls
-echo TheCommandLineScanner v1.0, made by weareblahs
-echo This defaults the scan settings to 300 dpi, Black and White.
-echo You can modify the script to change settings.
-echo Initializing...
-cd scans >nul
-del *.png >nul
-cd .. >nul
-mkdir scans >nul
+:: Program init
+echo Initialization in progress...
+:: Bring in the config.ini properties
+for /f "delims=" %%i in ('ini config.ini Main ScannerPDFDir') do set output=%%i >nul
+for /f "delims=" %%i in ('ini config.ini Main ScannerDPI') do set dpi=%%i >nul
+for /f "delims=" %%i in ('ini config.ini Main ScannerWidth') do set width=%%i >nul
+for /f "delims=" %%i in ('ini config.ini Main ScannerHeight') do set height=%%i >nul
+for /f "delims=" %%i in ('ini config.ini Main ScannerColor') do set color=%%i >nul
+for /f "delims=" %%i in ('ini config.ini Main ScannerTempDir') do set tempdir=%%i >nul
+for /f "delims=" %%i in ('ini config.ini Main ScannerTempOutFormat') do set fmt=%%i >nul
+for /f "delims=" %%i in ('ini config.ini Main Timer') do set timer=%%i >nul
+for /f "delims=" %%i in ('ini config.ini Main WaitTime') do set waittime=%%i >nul
+for /f "delims=" %%i in ('ini config.ini Main Pages') do set pages=%%i >nul
+
+:: Reset counter
 set /a result=0
-echo When you're ready, press any key.
+set /a pagecounter=0
+
+:: Program startup
+cls
+echo TheCommandLineScanner v1.1, made by weareblahs
+echo Modify your settings in the config.ini file.
+mkdir scans >nul
+mkdir temp >nul
+echo [1]: Normal Scan
+echo [2]: Scan with timer
+set /p check=Please select an option: 
+if %check%== 1 goto start
+if %check%== 2 goto start_timer
 pause >nul
 cls
 goto start
 
 :start
 echo Scanning page...
-:: Modify settings here
-wia-cmd-scanner /w 0 /h 0 /dpi 300 /color BW /format PNG /output "scans\%date:~4,2%%date:~7,2%%date:~10,4%%time:~0,2%%time:~3,2%%time:~6,2%.png" >nul
+wia-cmd-scanner /w %width% /h %height% /dpi %dpi% /color %color% /format %fmt% /output "temp\%date:~4,2%%date:~7,2%%date:~10,4%%time:~0,2%%time:~3,2%%time:~6,2%.png" >nul
 echo Please wait until the scanner finalizes scanning...
-timeout 2 >nul
+timeout %waittime% >nul
 cls
 set /a result=%result%+1
 echo %result% pages scanned.
@@ -31,11 +48,42 @@ if %check%== Y goto start
 if %check%== y goto start
 if %check%== N goto end
 
+:start_timer
+cls
+echo Pages can be set at config.ini at the "Pages" property.
+echo Press any key to start scanning.
+pause >nul
+goto autoscan
+
+:autoscan
+cls
+echo Scanning page...
+wia-cmd-scanner /w %width% /h %height% /dpi %dpi% /color %color% /format %fmt% /output "temp\%date:~4,2%%date:~7,2%%date:~10,4%%time:~0,2%%time:~3,2%%time:~6,2%.png" >nul
+echo Please wait until the scanner finalizes scanning...
+timeout %waittime% >nul
+cls
+set /a result=%result%+1
+set /a pagecounter=%pagecounter%+1
+echo %result% of %pages% pages scanned.
+echo Waiting for timer to finish...
+echo Currently, it is set to %timer% seconds.
+timeout %timer% >nul
+if %pagecounter%==%pages% goto end
+goto autoscan
 
 :end
 echo Scanning complete!
-set /p pdf=Type the PDF location / filename (if same folder of script) of the resulting PDF file. Please include ".pdf" at the end of the filename:
+set /p pdf=Type the PDF location / filename without the PDF file extension. The file will be saved in your selected directory at the config.ini file.: 
 cls
 echo Saving...
-magick scans\*.png "%pdf%"
+magick temp\*.png "%pdf%.pdf"
 echo Export to PDF complete!
+echo Press any key to exit.
+pause >nul
+echo Deleting temp files...
+cd scans >nul
+del *.png >nul
+del *.bmp >nul
+del *.tif >nul
+del *.tiff >nul
+del *.jpg >nul
